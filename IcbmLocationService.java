c8234130c89348a94132cc33b66d7990ba765623
Richard Yang <richardyangblog@gmail.com>
2017-09-14 19:34:10 -0400

    Delete IcbmLocationService.java

--- IcbmLocationService.java
+++
@@ -1,9 +0,0 @@
-package com.yang.icbm.location;
-
-public interface IcbmLocationService {
-
-    public IcbmLocation getLocation();
-    public IcbmLocation setLocation(IcbmLocation location);
-
-
-}
